93cf1342ae74b2293c88dd52a214ca4a24985511
Urivan <urivan07@gmail.com>
2022-05-15 10:43:31 -0500

    Prueba de build de express con netlify

--- src/server.js
+++ src/server.js
@@ -1,5 +1,8 @@
 const express = require("express");
+const serverless = require('serverless-http');
 const app = express();
+
+const router = express.Router();
 app.use(express.json());
 const port = process.env.PORT || 3000;
 
@@ -13,26 +16,26 @@ app.use(cors(corsOptions));
 const EspecieController = require("./controllers/EspecieController");
 const BiologoController = require("./controllers/BiologoController");
 
-app.get("/", (req, res) => {
+router.get("/", (req, res) => {
     res.json({message: "alive"});
 });
 
 
-app.get("/biologo", async (req, res) => { res.json(await(BiologoController.getList())); });
-app.get("/biologo/:id", async (req, res) => {res.json(await (BiologoController.getById(req.params.id)));});
-app.post("/biologo", async (req, res) => {res.json(await (BiologoController.create(req.body)));});
-app.put("/biologo/:id", async (req, res) => {res.json(await (BiologoController.update(req.params.id, req.body)));});
-app.delete("/biologo/:id", async (req, res) => {res.json(await (BiologoController.delete(req.params.id)));});
+router.get("/biologo", async (req, res) => { res.json(await(BiologoController.getList())); });
+router.get("/biologo/:id", async (req, res) => {res.json(await (BiologoController.getById(req.params.id)));});
+router.post("/biologo", async (req, res) => {res.json(await (BiologoController.create(req.body)));});
+router.put("/biologo/:id", async (req, res) => {res.json(await (BiologoController.update(req.params.id, req.body)));});
+router.delete("/biologo/:id", async (req, res) => {res.json(await (BiologoController.delete(req.params.id)));});
 
-app.get("/especies", async (req, res) => { res.json(await(EspecieController.getList())); });
-app.get("/especies/:id", async (req, res) => {res.json(await (EspecieController.getById(req.params.id)));});
-app.post("/especies", async (req, res) => {res.json(await (EspecieController.create(req.body)));});
-app.put("/especies/:id", async (req, res) => {res.json(await (EspecieController.update(req.params.id, req.body)));});
-app.delete("/especies/:id", async (req, res) => {res.json(await (EspecieController.delete(req.params.id)));});
+router.get("/especies", async (req, res) => { res.json(await(EspecieController.getList())); });
+router.get("/especies/:id", async (req, res) => {res.json(await (EspecieController.getById(req.params.id)));});
+router.post("/especies", async (req, res) => {res.json(await (EspecieController.create(req.body)));});
+router.put("/especies/:id", async (req, res) => {res.json(await (EspecieController.update(req.params.id, req.body)));});
+router.delete("/especies/:id", async (req, res) => {res.json(await (EspecieController.delete(req.params.id)));});
 
-var server = app.listen(port, () => {console.log(`Listening to requests on port ${port}`);});
-var handler = function() {
-    console.log("close server");
-    server.close();
-};
-module.exports = {app, handler};
+app.use(bodyParser.json());
+app.use('/.netlify/functions/server', router);  // path must route to lambda
+app.use('/', (req, res) => res.sendFile(path.join(__dirname, '../index.html')));
+
+module.exports = app;
+module.exports.handler = serverless(app);
